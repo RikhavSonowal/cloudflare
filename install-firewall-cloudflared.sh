7d053215de6c0e1333dcd6cbeef66531cd9598e2
Rikhav Sonowal <120777605+RikhavSonowal@users.noreply.github.com>
2025-06-09 07:47:40 +0530

    Update install-firewall-cloudflared.sh

--- install-firewall-cloudflared.sh
+++ install-firewall-cloudflared.sh
@@ -2,7 +2,6 @@
 sudo apt install ufw -y
 sudo ufw allow 22
 sudo ufw allow 3000/tcp
-sudo ufw enable
-
+echo y | sudo ufw enable
 wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
 sudo dpkg -i cloudflared-linux-amd64.deb
